e5e8ad098c954c81cfe40f0976dffa68301a74ea
lucasfeklein <lucas.fklein@hotmail.com>
2023-10-02 15:14:54 +0700

    start of the last part of content section

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Image, Text, Title } from "@mantine/core";
+import { Box, Button, Flex, Image, Text, Title } from "@mantine/core";
 import classes from "./Content.module.css";
 
 const cards = [
@@ -20,38 +20,68 @@ const cards = [
 
 function Content() {
   return (
-    <Box className={classes.content_container}>
-      <Box className={classes.main_content}>
-        <Image src="./image-web-3-desktop.jpg" />
-        <Box className={classes.title_container}>
-          <Title className={classes.title}>The Bright Future of Web 3.0?</Title>
-          <Box
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text color="hsl(236, 13%, 42%)">
-              We dive into the next evolution of the web that claims to put the
-              power of the platforms back into the hands of the prople. But is
-              it really fulfilling its promise?
-            </Text>
-            <Button className={classes.button}>READ MORE</Button>
+    <Box>
+      <Box className={classes.content_container}>
+        <Box className={classes.main_content}>
+          <Image src="./image-web-3-desktop.jpg" />
+          <Box className={classes.title_container}>
+            <Title className={classes.title}>
+              The Bright Future of Web 3.0?
+            </Title>
+            <Box
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text color="hsl(236, 13%, 42%)">
+                We dive into the next evolution of the web that claims to put
+                the power of the platforms back into the hands of the prople.
+                But is it really fulfilling its promise?
+              </Text>
+              <Button className={classes.button}>READ MORE</Button>
+            </Box>
           </Box>
         </Box>
+        <Box p={20} className={classes.news_container}>
+          <Title size="35px" style={{ color: "hsl(35, 77%, 62%)" }} order={2}>
+            New
+          </Title>
+          {cards.map((card) => (
+            <Box key={card.title} className={classes.news_card}>
+              <Text className={classes.news_subtitle}>{card.title}</Text>
+              <Text className={classes.news_paragraph}>{card.subtitle}</Text>
+            </Box>
+          ))}
+        </Box>
       </Box>
-      <Box p={20} className={classes.news_container}>
-        <Title size="35px" style={{ color: "hsl(35, 77%, 62%)" }} order={2}>
-          New
-        </Title>
-        {cards.map((card) => (
-          <Box key={card.title} className={classes.news_card}>
-            <Text className={classes.news_subtitle}>{card.title}</Text>
-            <Text className={classes.news_paragraph}>{card.subtitle}</Text>
+      <Flex wrap="wrap" mt={50}>
+        <Flex w={350}>
+          <Image src="./image-retro-pcs.jpg" height={120} />
+          <Box>
+            <Text>01</Text>
+            <Title order={3}>Reviving Retro PCs</Title>
+            <Text>What happens when old PCs are given modern upgrades?</Text>
           </Box>
-        ))}
-      </Box>
+        </Flex>
+        <Flex w={350}>
+          <Image src="./image-retro-pcs.jpg" height={120} />
+          <Box>
+            <Text>01</Text>
+            <Title order={3}>Reviving Retro PCs</Title>
+            <Text>What happens when old PCs are given modern upgrades?</Text>
+          </Box>
+        </Flex>
+        <Flex w={350}>
+          <Image src="./image-retro-pcs.jpg" height={120} />
+          <Box>
+            <Text>01</Text>
+            <Title order={3}>Reviving Retro PCs</Title>
+            <Text>What happens when old PCs are given modern upgrades?</Text>
+          </Box>
+        </Flex>
+      </Flex>
     </Box>
   );
 }
