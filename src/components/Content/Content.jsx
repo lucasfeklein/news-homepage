e371e0d46792f789753e8f88c15de1fb606c690f
lucasfeklein <lucas.fklein@hotmail.com>
2023-10-02 14:13:12 +0700

    main content styles done

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -6,15 +6,21 @@ function Content() {
     <Box className={classes.content_container}>
       <Box className={classes.main_content}>
         <Image src="./image-web-3-desktop.jpg" />
-        <Box className={classes.title_container}>
-          <Title className={classes.title}>The Brigth Future of Web 3.0?</Title>
-          <Box>
-            <Text>
+        <Box className={classes.title_container} mt={20}>
+          <Title className={classes.title}>The Bright Future of Web 3.0?</Title>
+          <Box
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text color="hsl(236, 13%, 42%)">
               We dive into the next evolution of the web that claims to put the
               power of the platforms back into the hands of the prople. But is
               it really fulfilling its promise?
             </Text>
-            <Button>READ MORE</Button>
+            <Button className={classes.button}>READ MORE</Button>
           </Box>
         </Box>
       </Box>
