bd66018dbc08782f65c726444a1cd7b1afe939dd
lucasfeklein <lucas.fklein@hotmail.com>
2023-10-03 16:24:18 +0700

    slide in and slide out working

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -37,50 +37,52 @@ function Header() {
           </List.Item>
         </List>
       )}
-      {toggleMenu &&
-        size.width <= 800 && ( // Render the fixed menu when the icon-menu is clicked
-          <div className={classes.fullscreenOverlay}>
-            <div
-              className={`${classes.fixedMenu} ${
-                toggleMenu ? classes.active : ""
-              }`}
+      {size.width <= 800 && ( // Render the fixed menu when the icon-menu is clicked
+        <div
+          className={classes.fullscreenOverlay}
+          style={{ visibility: toggleMenu ? "" : "hidden" }}
+        >
+          <div
+            className={`${classes.fixedMenu} ${
+              toggleMenu ? classes.active : ""
+            }`}
+          >
+            <Image
+              src="./icon-menu-close.svg"
+              w={40}
+              style={{
+                alignSelf: "end",
+                cursor: "pointer",
+                marginBottom: "150px",
+              }}
+              onClick={() => setToggleMenu(!toggleMenu)}
+            />
+            <List
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "15px",
+              }}
             >
-              <Image
-                src="./icon-menu-close.svg"
-                w={40}
-                style={{
-                  alignSelf: "end",
-                  cursor: "pointer",
-                  marginBottom: "150px",
-                }}
-                onClick={() => setToggleMenu(!toggleMenu)}
-              />
-              <List
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "15px",
-                }}
-              >
-                <List.Item className={classes.list_item}>
-                  <Anchor className={classes.anchor}>Home</Anchor>
-                </List.Item>
-                <List.Item className={classes.list_item}>
-                  <Anchor className={classes.anchor}>New</Anchor>
-                </List.Item>
-                <List.Item className={classes.list_item}>
-                  <Anchor className={classes.anchor}>Popular</Anchor>
-                </List.Item>
-                <List.Item className={classes.list_item}>
-                  <Anchor className={classes.anchor}>Trending</Anchor>
-                </List.Item>
-                <List.Item className={classes.list_item}>
-                  <Anchor className={classes.anchor}>Categories</Anchor>
-                </List.Item>
-              </List>
-            </div>
+              <List.Item className={classes.list_item}>
+                <Anchor className={classes.anchor}>Home</Anchor>
+              </List.Item>
+              <List.Item className={classes.list_item}>
+                <Anchor className={classes.anchor}>New</Anchor>
+              </List.Item>
+              <List.Item className={classes.list_item}>
+                <Anchor className={classes.anchor}>Popular</Anchor>
+              </List.Item>
+              <List.Item className={classes.list_item}>
+                <Anchor className={classes.anchor}>Trending</Anchor>
+              </List.Item>
+              <List.Item className={classes.list_item}>
+                <Anchor className={classes.anchor}>Categories</Anchor>
+              </List.Item>
+            </List>
           </div>
-        )}
+        </div>
+      )}
     </Box>
   );
 }
